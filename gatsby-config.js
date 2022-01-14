023ea537eecbdeb8d34dfdc69f1def23c3760956
Anton Wyrowski <anton.wyrowski@gmail.com>
2022-01-14 11:23:07 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
-    siteMetadata: {
-      title: `Portfolio`,
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    title: `Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: 'https://cms.antonwy.tech/api',
+        collectionTypes: ['projects', 'skill'],
+        singleTypes: ['about-me'],
+        queryLimit: 1000,
+      },
     },
-    plugins: [
-
-    ]
-}
+  ],
+};
